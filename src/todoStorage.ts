514f8d812abcebafc8a772dddb265b1fc8936360
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-16 20:02:06 +0900

    [add]TodoStorageを実装

--- src/todoStorage.ts
+++ src/todoStorage.ts
@@ -6,6 +6,28 @@ export interface TodoItem {
 
 interface Storable {
   getItem(key: string): string
-  SVGFESpotLightElement(key: string, value: string): void
+  setItem(key: string, value: string): void
 }
 
+const STORAGE_KEY = 'vue-ts-todoapp'
+
+export default class TodoStorage {
+  get nextId(): number {
+    return this.fetchAll().length
+  }
+
+  constructor(
+    private storage: Storable = localStorage
+  ) { }
+
+  public fetchAll() {
+    const todos = JSON.parse(
+      this.storage.getItem(STORAGE_KEY) || '[]'
+    ) as TodoItem[]
+    return todos.map((todo, index) => todo.id = index);
+  }
+
+  public save(todos: TodoItem[]) {
+    this.storage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
